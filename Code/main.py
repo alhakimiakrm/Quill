f87c8e7820c54686563b7420f45acaf3cce3b324
ace al-hakimi <alhakimiakrmj@gmail.com>
2024-03-12 06:24:04 -0400

    Trying to get sample texts loaded in

--- Code/main.py
+++ Code/main.py
@@ -1,18 +1,35 @@
-import spacy
 import nltk
 from textblob import TextBlob
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
 
-# Get user input
-user_input = input("Enter your text: ")
 
-# Create a TextBlob object
-blob = TextBlob(user_input)
+#load english model from spacy library
+nlp = spacy.load("en_core_web_sm") 
 
-# Tokenize and tag parts of speech
-for word, tag in blob.tags:
-    print(word, tag)
 
-# Analyze sentiment
-sentiment = blob.sentiment
-print("Polarity: ", sentiment.polarity)
-print("Subjectivity: ", sentiment.subjectivity)
+def create_lang_detector(nlp, name):
+    return LanguageDetector()
+
+#add lang detector to pipeline
+Language.factory("language_detector", func=create_lang_detector)
+nlp.add_pipe("language_detector", last=True)
+
+#test
+sample_text = input('Enter text: ')
+doc = nlp(sample_text)
+
+
+def analyze_text(text):
+    blob = TextBlob(text)
+    return blob.sentiment
+
+def read(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+    
+hemingwaydir = os.path.join('..', TextSamples)
+
+
+print(analyze_text(doc.text))
