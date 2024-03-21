431ff9dc85af567e6b79221d46aa853b47859c35
Akrm (Ace) Al-Hakimi <111914307+alhakimiakrm@users.noreply.github.com>
2024-03-21 15:25:32 -0400

    Delete Code/word2vec.py

--- Code/word2vec.py
+++
@@ -1,5 +0,0 @@
-from gensim import Word2Vec
-from main import all_sentences
-
-model = Word2Vec(sentences=all_sentences, vector_size=100, window=5, min_count=1, workers=4)
-model.save("hemingway_word2vec.model")
